40f1369fa0e58cbf32a847dd79bac0af7c64b50c
faun3 <antonioculcea@gmail.com>
2023-10-11 16:52:16 +0300

    okay

--- pb3/pb3.cpp
+++ pb3/pb3.cpp
@@ -2,10 +2,19 @@
 
 class Car {
 private:
-    char sellerName[30];
+    int* kmsDrivenSnapshots;
+    char pastOwners[10][60];
+    int nrOfPastOwners;
     enum Make {Toyota, Dacia, Tesla, Volkswagen};
     Make make;
     bool isRefurbished;
+public:
+    Car() {
+        kmsDrivenSnapshots = nullptr;
+        int nrOfPastOwners = 0;
+        make = Toyota;
+        isRefurbished = false;
+    }
 };
 
 int main()
